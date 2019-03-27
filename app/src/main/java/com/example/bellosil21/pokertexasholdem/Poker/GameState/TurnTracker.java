ade46d6bccd77351b346e2fac6d305d5f0ac0d2b
hoser21 <hoser21@up.edu>
2019-03-27 09:05:26 -0700

    Made instance vars private

--- app/src/main/java/com/example/bellosil21/pokertexasholdem/Poker/GameState/TurnTracker.java
+++ app/src/main/java/com/example/bellosil21/pokertexasholdem/Poker/GameState/TurnTracker.java
@@ -12,19 +12,23 @@ import java.util.ArrayList;
  * @author Gabe Marcial
  */
 public class TurnTracker {
-    ArrayDeque<Integer> activePlayers; // players who haven't taken a turn in this phase
-    ArrayDeque<Integer> promptedPlayers; // players who taken a turn in this phase
-    ArrayList<Integer> foldedPlayers; // players who folded and no longer in this round
-    ArrayList<Integer> sittingOutPlayers; // players who are sitting out and fold on the first phase
-    ArrayList<Integer> removedPlayers; // players who left the game or no longer have any chips
+    private ArrayDeque<Integer> activePlayers; // players who haven't taken a
+    // turn in this phase
+    private ArrayDeque<Integer> promptedPlayers; // players who taken a turn in
+    // this phase
+    private ArrayList<Integer> foldedPlayers; // players who folded and no
+    // longer in this round
+    private ArrayList<Integer> sittingOutPlayers; // players who are sitting out
+    // and fold on the first phase
+    private ArrayList<Integer> removedPlayers; // players who left the game or
+    // no longer have any chips
     // to bet (an amount of 0)
-    int numPlayers; // the number of players in this game
-    int dealerID; // is a marker for blinds and who takes the first turn when dealerID ==
-    // playerID, the player is smallBlind
+    private int numPlayers; // the number of players in this game
+    private int dealerID; // is a marker for blinds and who takes the first
+    // turn when dealerID == playerID, the player is smallBlind
     // (dealerID + 1) % numPlayers == playerID, the player is bigBlind
     // (dealerID + 2) % numPlayers == playerID, the player takes the first turn
-
-    /**
+    /*
      * External Citation
      *  Date:     26 March 2019
      *  Problem:  Did not know java's LinkedList implementation.
