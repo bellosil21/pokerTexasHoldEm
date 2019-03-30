ee4b032667485d833eacf76f2fefebb0f81c7a58
marcial21 <marcial21@up.edu>
2019-03-29 19:09:16 -0700

    implemented game configuration in the PokerMainActivity class.

--- app/src/main/java/com/example/bellosil21/pokertexasholdem/Poker/PokerMainActivity.java
+++ app/src/main/java/com/example/bellosil21/pokertexasholdem/Poker/PokerMainActivity.java
@@ -1,9 +1,13 @@
 package com.example.bellosil21.pokertexasholdem.Poker;
 
 import com.example.bellosil21.pokertexasholdem.Game.GameMainActivity;
+import com.example.bellosil21.pokertexasholdem.Game.GamePlayer;
 import com.example.bellosil21.pokertexasholdem.Game.LocalGame;
 import com.example.bellosil21.pokertexasholdem.Game.config.GameConfig;
 import com.example.bellosil21.pokertexasholdem.Game.config.GamePlayerType;
+import com.example.bellosil21.pokertexasholdem.Poker.GameState.PokerLocalGame;
+import com.example.bellosil21.pokertexasholdem.Poker.Player.PokerDumbComputerPlayer;
+import com.example.bellosil21.pokertexasholdem.Poker.Player.PokerHumanPlayer;
 
 import java.util.ArrayList;
 
@@ -16,12 +20,41 @@ public class PokerMainActivity extends GameMainActivity {
     @Override
     public GameConfig createDefaultConfig() {
         //define player types?
-        ArrayList<GamePlayerType>
-        return null;
+        ArrayList<GamePlayerType> playerTypes = new ArrayList<GamePlayerType>();
+
+        playerTypes.add(new GamePlayerType("Human player") {
+            @Override
+            public GamePlayer createPlayer(String name) {
+                return new PokerHumanPlayer(name);
+            }
+        });
+
+        playerTypes.add(new GamePlayerType("Computer player (dumb like you)") {
+            @Override
+            public GamePlayer createPlayer(String name) {
+                return new PokerDumbComputerPlayer(name);
+            }
+        });
+
+        //create a game configuration 'class' for texas boldem.
+        GameConfig defaultConfig = new GameConfig(playerTypes, 2, 4,
+                "Texas Holdem", PORT_NUMBER);
+
+        //Add the default players
+        defaultConfig.addPlayer("Human", 0); // this index represents the index in
+        //the playerTypes array list.
+        defaultConfig.addPlayer("Dumb Computer", 1);
+        defaultConfig.addPlayer("Dumb Computer2", 1);
+        defaultConfig.addPlayer("Dumb Computer3", 1);
+
+        //set the initial information for the remote player?
+        defaultConfig.setRemoteData("Poker Guest", "Some IP code",
+                0); //not really sure what this does.
+        return defaultConfig;
     }
 
     @Override
     public LocalGame createLocalGame() {
-        return null;
+        return new PokerLocalGame();
     }
 }
