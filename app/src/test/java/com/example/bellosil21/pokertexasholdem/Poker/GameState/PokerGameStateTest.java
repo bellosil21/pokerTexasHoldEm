2a61cbae6895d3f0e0d05afa4c28320b907c88a8
Kevin Hoser <hoser21@up.edu>
2019-04-16 19:28:29 -0700

    Add unit tests

--- app/src/test/java/com/example/bellosil21/pokertexasholdem/Poker/GameState/PokerGameStateTest.java
+++ app/src/test/java/com/example/bellosil21/pokertexasholdem/Poker/GameState/PokerGameStateTest.java
@@ -1,11 +1,8 @@
 package com.example.bellosil21.pokertexasholdem.Poker.GameState;
 
-import com.example.bellosil21.pokertexasholdem.Poker.Hand.Card;
 import com.example.bellosil21.pokertexasholdem.Poker.Hand.Deck;
 
 import org.junit.Test;
-
-import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
@@ -23,13 +20,16 @@ public class PokerGameStateTest {
 
     @Test
     public void deal() {
-        PokerGameState myGameState = new PokerGameState(0, 100, 200, 4);
-        Deck myDeck = myGameState.getDeck();
-        assertEquals(null, myDeck);
-        myGameState.deal();
-        assertNotEquals(null, myDeck);
-        ArrayList<Card> cardArrayList = new ArrayList<>();
+        dealHealer(1);
+        dealHealer(4);
+    }
 
+    private void dealHealer(int numPlayers) {
+        PokerGameState myGameState = new PokerGameState(0, 100, 200, numPlayers);
+        Deck myDeck = myGameState.getDeck();
+        assertEquals(52, myDeck.getDeck().size()); // a new deck has 52 cards
+        myGameState.deal();
+        assertNotEquals(52 - numPlayers * 2, myDeck);
     }
 
     @Test
